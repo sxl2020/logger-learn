b796ac7c93a52dcfe92f89e203d5ad9be90872c8
sxl2020 <suqiancheng_su@163.com>
2021-11-15 18:58:12 +0800

    简单的日志打印

---
+++ hello-logger/src/main/java/com/sxl/hellologger/HelloLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.sxl.hellologger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloLoggerApplication.class, args);
+    }
+
+}
---
+++ hello-logger/src/test/java/com/sxl/hellologger/HelloLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxl.hellologger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
